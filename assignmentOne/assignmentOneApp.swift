b1c73341658653b1d955d8292d38be41dd3cb35a
jeremy <jeremyninety@gmail.com>
2024-02-25 17:06:55 -0500

    Initial Commit

---
+++ assignmentOne/assignmentOneApp.swift
@@ -0,0 +1,17 @@
+//
+//  assignmentOneApp.swift
+//  assignmentOne
+//
+//  Created by Jeremy Barnes-Smith on 2/25/24.
+//
+
+import SwiftUI
+
+@main
+struct assignmentOneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
